476048b76278ed8e3e67b52f5a808694bffd8452
Emrick19 <emricky17@gmail.com>
2021-10-13 22:12:51 +0100

    Update Project section

--- src/components/modules/Projects/index.tsx
+++ src/components/modules/Projects/index.tsx
@@ -1,51 +1,47 @@
 import { useTheme } from 'providers/ThemeProvider';
-import { RepositoryEdge } from 'generated/graphql';
 import { Card, TitleWrap } from 'components/ui/Card';
 import Container from 'components/ui/Container';
 import Fork from 'components/ui/Icons/Fork';
 import Star from 'components/ui/Icons/Star';
-import { Wrapper, Grid, Item, Content, Stats, Languages } from './styles';
+import { Wrapper, Grid, Item, Content, Stats, ViewLive } from './styles';
+import projects from './projects.json';
 
-const Projects = ({ data }: { data: RepositoryEdge[] }) => {
+const Projects = () => {
   const { theme } = useTheme();
 
   return (
     <Wrapper as={Container} id="projects">
       <h2>Projects</h2>
       <Grid>
-        {data &&
-          data.map(({ node }) => {
-            if (!node) return null;
-            return (
-              <Item key={node.id} as="a" href={node.url} target="_blank" rel="noopener noreferrer" theme={theme}>
-                <Card theme={theme}>
-                  <Content>
-                    <h3>{node.name}</h3>
-                    <p>{node.description}</p>
-                  </Content>
-                  <TitleWrap>
-                    <Stats theme={theme} stars>
-                      <div>
-                        <Star color={theme === 'light' ? '#000' : '#fff'} />
-                        <span>{node.stargazers.totalCount}</span>
-                      </div>
-                      <div>
-                        <Fork color={theme === 'light' ? '#000' : '#fff'} />
-                        <span>{node.forkCount}</span>
-                      </div>
-                    </Stats>
-                    <Stats theme={theme}>
-                      <Languages theme={theme}>
-                        {node.languages!.nodes!.map((item) => (
-                          <span key={item!.id}>{item!.name}</span>
-                        ))}
-                      </Languages>
-                    </Stats>
-                  </TitleWrap>
-                </Card>
-              </Item>
-            );
-          })}
+        {projects.map((project) => {
+          if (!project) return null;
+          return (
+            <Item key={project.id} as="a" href={project.link} target="_blank" rel="noopener noreferrer" theme={theme}>
+              <Card theme={theme}>
+                <Content>
+                  <h3>{project.name}</h3>
+                  <p>{project.description}</p>
+                </Content>
+                <img src={project.imageUrl} alt="Picture of the author" />
+                <TitleWrap>
+                  <Stats theme={theme} stars>
+                    <div>
+                      <Star color={theme === 'light' ? '#000000' : '#fff'} />
+                    </div>
+                    <div>
+                      <Fork color={theme === 'light' ? '#000' : '#fff'} />
+                    </div>
+                  </Stats>
+                  {project.showViewLive ? (
+                    <ViewLive as="a" href={project.link} target="_blank" rel="noopener noreferrer">
+                      {<span key={project.id}>view live</span>}
+                    </ViewLive>
+                  ) : null}
+                </TitleWrap>
+              </Card>
+            </Item>
+          );
+        })}
       </Grid>
     </Wrapper>
   );
