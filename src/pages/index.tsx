a70196afe1a726c95451349783ff628893774d0d
Emrick19 <emricky17@gmail.com>
2021-10-13 22:18:31 +0100

    Terminat api call

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,3 @@
-import axios from 'axios';
-import { GetStaticProps } from 'next';
-import { RepositoryEdge } from 'generated/graphql';
 import Layout from 'components/ui/Layout';
 import SEO from 'components/SEO';
 import Intro from 'components/modules/Intro';
@@ -8,61 +5,14 @@ import Projects from 'components/modules/Projects';
 import Skills from 'components/modules/Skills';
 import Contact from 'components/modules/Contact';
 
-const HomePage = ({ repos }: { repos: RepositoryEdge[] }) => (
+const HomePage = () => (
   <Layout>
     <SEO />
     <Intro />
-    <Projects data={repos} />
+    <Projects />
     <Skills />
     <Contact />
   </Layout>
 );
-
-export const getStaticProps: GetStaticProps = async () => {
-  const {
-    data: { data },
-  } = await axios({
-    url: 'https://api.github.com/graphql',
-    method: 'post',
-    data: {
-      query: `
-				query viewer {
-					viewer {
-						repositories(first: 8, orderBy: {field: STARGAZERS, direction: DESC}) {
-							edges {
-								node {
-									id
-									name
-									url
-									description
-									stargazers {
-										totalCount
-									}
-									forkCount
-									languages(first: 3) {
-										nodes {
-											id
-											name
-										}
-									}
-								}
-							}
-						}
-					}
-				}
-			`,
-    },
-    headers: {
-      Authorization: `bearer ${process.env.GITHUB_TOKEN}`,
-    },
-  });
-
-  return {
-    props: {
-      repos: data.viewer.repositories.edges,
-    },
-    revalidate: 10,
-  };
-};
 
 export default HomePage;
